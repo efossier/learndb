2daabdf44331d16aa988b3c2611b35febb3f8b8d
efossier <evan.fossier@gmail.com>
2018-12-31 13:57:56 -0800

    Simple impl

--- src/key-value-store.js
+++ src/key-value-store.js
@@ -1,1 +1,25 @@
-export class KeyValueStore {}
+export class KeyValueStore {
+  constructor() {
+    this.store = {}
+  }
+
+  init() {}
+
+  set(key, val) {
+    this.store[key] = val
+  }
+
+  get(key) {
+    return this.store[key]
+  }
+
+  delete(key) {
+    if (this.get(key) == undefined) 
+      return false
+      
+    this.store[key] = undefined
+    return true
+  }
+
+  checkAndSet() {}
+}
--- tests/key-value-store.js
+++ tests/key-value-store.js
@@ -1,10 +1,60 @@
 import chai, { assert } from 'chai'
 import chaiString from 'chai-string'
 
+import { KeyValueStore } from '../src/key-value-store'
+
 chai.use(chaiString)
 
 describe('KeyValueStore', () => {
-  it('tests work', () => {
-    assert.isTrue(true, 'yup, true is true')
+  // Test keys and values we'll use in the new tests
+  const testKey1 = 'test-key-1'
+  const testValue1 = 'test-value-1'
+  const testValue2 = 'test-value-2'
+
+  // Contains a fresh instance of the key-value store for each test.
+  let keyValueStore = null
+
+  beforeEach(() => {
+    // Before each test, create a new instance of the key-value store.
+    keyValueStore = new KeyValueStore()
+
+    // This won't do anything for now, but will be helpful soon when we have
+    // to initialize resources such as database files.
+    keyValueStore.init()
+  })
+
+  it('get() returns value that was set()', () => {
+    keyValueStore.set(testKey1, testValue1)
+    assert.equal(keyValueStore.get(testKey1), testValue1)
+  })
+  
+  it('get() returns last value that was set()', () => {
+    keyValueStore.set(testKey1, testValue1)
+    keyValueStore.set(testKey1, testValue2)
+    assert.equal(keyValueStore.get(testKey1), testValue2)
+  })
+  
+  it('get() for non-existent key returns undefined', () => {
+    assert.equal(keyValueStore.get(testKey1), undefined)
+  })
+
+  it('set() and get() support null value', () => {
+    keyValueStore.set(testKey1, null)
+    assert.equal(keyValueStore.get(testKey1), null)
+  })
+  
+  it('delete() for key causes get() to return undefined', () => {
+    keyValueStore.set(testKey1, testValue1)
+    keyValueStore.delete(testKey1)
+    assert.equal(keyValueStore.get(testKey1), undefined)
+  })
+
+  it('delete() returns true if key existed', () => {
+    keyValueStore.set("abc", "def")
+    assert.equal(keyValueStore.delete("abc"), true)
+  })
+
+  it('delete() returns false if key did not exist', () => {
+    assert.equal(keyValueStore.delete("abc"), false)
   })
 })
